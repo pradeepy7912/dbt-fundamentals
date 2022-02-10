f5f9bf9fd475e39ec9889c2001cb280c25795062
dyarlagadda@itsmarta.com <dyarlagadda@itsmarta.com>
2022-02-10 18:49:33 +0000

    created dim_customers

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,28 +1,15 @@
 {{
    config(materialized="table")
 }}
+
+
 with customers as (
+    
+    select * from {{ ref('stg_customers') }} 
+    
+    ),
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-
-),
-
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
-
-),
+orders as (select * from {{ ref('stg_orders') }} ),
 
 customer_orders as (
 
